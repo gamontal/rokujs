855a4e867d935ed251e6f4b56a06ec92267b82c9
Cole Hafner <colehafner@gmail.com>
2020-01-05 21:39:06 -0800

    feat: started advanced CLI

--- cli.js
+++ cli.js
@@ -1,62 +1,99 @@
 const Roku = require('./lib/roku');
 
+const fs = require('fs');
+
+const doConfig = () => {
+    const config = {
+        selected: '',
+        format: 'pretty',
+    };
+
+    const readFile = file => {
+        if (!fs.existsSync(file)) {
+            return false;
+        } 
+        
+        return JSON.parse(fs.readFileSync(file));
+    }
+
+    const writeFile = (path, json) => fs.writeFileSync(path, json);
+    const configFile = '.roku.json'
+
+    let newConfig = readFile(`./${configFile}`);
+
+    if (newConfig === false) {
+        newConfig = readFile(`~/${configFile}`);
+    }
+
+
+}
+
 const arg1 = (process.argv[2] || '').toLowerCase(); 
 const arg2 = (process.argv[3] || '');
 const arg3 = (process.argv[4] || '');
 
 switch(arg1) {
-    case 'discover':
-    case 'ls':
-        Roku.discover(console.log)
-        break;
+    case 'config':
+        // doConfig
 
-    
-    case '':
-    case 'h':
-    case 'help':
         break;
 
     default:
-        if (/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(arg1) === false) {
-            console.log('Send device IP address! Available devices are... \n\n');
-            Roku.discover(devices => {
-                console.log(devices); 
-                process.exit();
-            });
-        }else {
-            const device = new Roku(arg1);
+        switch(arg2) {
+            case 'discover':
+            case 'ls':
+                Roku.discover(console.log)
+                break;
 
-            switch(arg2) {
-                case '':
-                case 'ls':
-                case 'apps':
-                    device.apps((err, apps) => console.log(apps));
-                    break;
+            
+            case '':
+            case 'h':
+            case 'help':
+                break;
 
-                case 'active':
-                case 'active-app':
-                    device.apps({active: true}, (err, apps) => console.log(apps));
-                    break;
-
-                case 'info':
-                    device.deviceInfo(info => console.log(JSON.stringify(info)));
-                    break;
-
-                case 'launch':
-                    const key = /\d+/.test(arg3) === true ? 'id': 'name';
-                    device.launch({[key]: arg3}, err => {
-                        if (err) console.log(`Error launching app "${arg3}"`);
+            default:
+                if (/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(arg1) === false) {
+                    console.log('Send device IP address! Available devices are... \n\n');
+                    Roku.discover(devices => {
+                        console.log(devices); 
+                        process.exit();
                     });
-                    break;
+                }else {
+                    const device = new Roku(arg1);
 
-                default:
-                    if (Roku.keys.includes(arg2)) {
-                        device.press(arg2);
-                    }else {
-                        console.log(`Unknown command "${arg2}"`)
+                    switch(arg2) {
+                        case '':
+                        case 'ls':
+                        case 'apps':
+                            device.apps((err, apps) => console.log(apps));
+                            break;
+
+                        case 'active':
+                        case 'active-app':
+                            device.apps({active: true}, (err, apps) => console.log(apps));
+                            break;
+
+                        case 'info':
+                            device.deviceInfo(info => console.log(JSON.stringify(info)));
+                            break;
+
+                        case 'launch':
+                            const key = /\d+/.test(arg3) === true ? 'id': 'name';
+                            device.launch({[key]: arg3}, err => {
+                                if (err) console.log(`Error launching app "${arg3}"`);
+                            });
+                            break;
+
+                        default:
+                            if (Roku.keys.includes(arg2)) {
+                                device.press(arg2);
+                            }else {
+                                console.log(`Unknown command "${arg2}"`)
+                            }
+                            break;
                     }
-                    break;
-            }
+                }
+                break;
         }
         break;
 }
